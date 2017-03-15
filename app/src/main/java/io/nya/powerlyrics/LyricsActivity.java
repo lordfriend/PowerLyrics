02a30635884e3bdf837f748f89210045d62bb12d
lordfriend <lordfriend@outlook.com>
2017-03-15 22:23:48 +0800

    change the track method. still have bug in lock screen and unlock transition

--- app/src/main/java/io/nya/powerlyrics/LyricsActivity.java
+++ app/src/main/java/io/nya/powerlyrics/LyricsActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +15,6 @@ import android.widget.TextView;
 import com.maxmpz.poweramp.player.PowerampAPI;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 import io.nya.powerlyrics.lyric.Lyric;
 import io.nya.powerlyrics.lyric.LyricParser;
@@ -22,13 +22,8 @@ import io.nya.powerlyrics.model.PlayStatus;
 import io.nya.powerlyrics.model.Track;
 import io.nya.powerlyrics.service.PlayService;
 import io.nya.powerlyrics.view.LyricView;
-import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.functions.Function;
-import io.reactivex.functions.Predicate;
-import io.reactivex.observables.ConnectableObservable;
 import io.reactivex.observers.DisposableObserver;
 
 
@@ -40,10 +35,6 @@ public class LyricsActivity extends Activity {
      * the interval to update play position
      */
     private static final long DEFAULT_INTERVAL = 400;
-    /**
-     * whenever a play status changes to pause or stop. invalid current position
-     */
-    private static final long INVALID_POSITION = -1;
 
     private LyricApplication mApp;
     private CompositeDisposable mDisposable = new CompositeDisposable();
@@ -51,12 +42,22 @@ public class LyricsActivity extends Activity {
     private Track mCurrentTrack;
     private long mCurrentTrackPos;
     private long mLastSyncTimestamp;
-    private PlayStatus mPlayStatus;
 
     TextView mStateIndicator;
     ViewGroup mMainContainer;
     LyricView mLyricView;
     TextView mTrackTitleView;
+
+    private Handler mHandler = new Handler();
+
+    private Runnable mTickRunnable = new Runnable() {
+        @Override
+        public void run() {
+            long pos = System.currentTimeMillis() - mLastSyncTimestamp + mCurrentTrackPos;
+            mLyricView.updateCurrentTime(pos);
+            mHandler.postDelayed(this, DEFAULT_INTERVAL);
+        }
+    };
 
     private void updateTrackPosition(int position) {
         mCurrentTrackPos = position * 1000;
@@ -74,52 +75,16 @@ public class LyricsActivity extends Activity {
     };
 
     private void startTrackPosition() {
+        if (mCurrentTrack.lyric_status != Track.LyricStatus.FOUND) {
+            return;
+        }
         // immediately sync the position of track.
         startService(PowerampAPI.newAPIIntent().putExtra(PowerampAPI.COMMAND, PowerampAPI.Commands.POS_SYNC));
+        mHandler.postDelayed(mTickRunnable, 0);
+    }
 
-        mDisposable.add(mApp.mStatusSubject
-                .map(new Function<PlayStatus, Long>() {
-                    @Override
-                    public Long apply(@NonNull PlayStatus status) throws Exception {
-                        Log.d(TAG, "playStatus: " + mPlayStatus);
-                        mPlayStatus = status;
-                        return 0L;
-                    }
-                })
-                .mergeWith(Observable.interval(DEFAULT_INTERVAL, TimeUnit.MILLISECONDS))
-                .filter(new Predicate<Long>() {
-                    @Override
-                    public boolean test(@NonNull Long aLong) throws Exception {
-                        Log.d(TAG, "predicate result: " + (mPlayStatus != null && mPlayStatus.status == PowerampAPI.Status.TRACK_PLAYING && !mPlayStatus.isPaused));
-                        return mPlayStatus != null && mPlayStatus.status == PowerampAPI.Status.TRACK_PLAYING && !mPlayStatus.isPaused;
-                    }
-                })
-                .map(new Function<Long, Long>() {
-                    @Override
-                    public Long apply(@NonNull Long aLong) throws Exception {
-                        Log.d(TAG, "pos=" + (System.currentTimeMillis() - mLastSyncTimestamp + mCurrentTrackPos));
-                        return System.currentTimeMillis() - mLastSyncTimestamp + mCurrentTrackPos;
-                    }
-                })
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeWith(new DisposableObserver<Long>() {
-                    @Override
-                    public void onNext(Long pos) {
-                        mLyricView.updateCurrentTime(pos);
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        Log.e(TAG, "track error: " + e.toString());
-                        mCurrentTrackPos = INVALID_POSITION;
-                    }
-
-                    @Override
-                    public void onComplete() {
-                        Log.d(TAG, "Track complete");
-                        mCurrentTrackPos = INVALID_POSITION;
-                    }
-                }));
+    private void stopTrackPosition() {
+        mHandler.removeCallbacks(mTickRunnable);
     }
 
     private void setLyric(String lyricStr) {
@@ -133,17 +98,6 @@ public class LyricsActivity extends Activity {
             mStateIndicator.setText(getResources().getString(R.string.lrc_parse_error));
             e.printStackTrace();
         }
-    }
-
-    private void trackPlayerStatus() {
-        registerReceiver(mTrackPosSyncReceiver, new IntentFilter(PowerampAPI.ACTION_TRACK_POS_SYNC));
-//        registerReceiver(mStatsChangeReceiver, new IntentFilter(PowerampAPI.ACTION_STATUS_CHANGED));
-        startTrackPosition();
-    }
-
-    private void stopTrackPlayerStatus() {
-        unregisterReceiver(mTrackPosSyncReceiver);
-//        unregisterReceiver(mStatsChangeReceiver);
     }
 
     private void handleIntent(Intent intent) {
@@ -183,6 +137,7 @@ public class LyricsActivity extends Activity {
     @Override
     protected void onResume() {
         Log.w(TAG, "onResume");
+        registerReceiver(mTrackPosSyncReceiver, new IntentFilter(PowerampAPI.ACTION_TRACK_POS_SYNC));
         mDisposable.add(mApp.mCurrentTrackSubject.subscribeWith(new DisposableObserver<Track>() {
             @Override
             public void onNext(Track track) {
@@ -190,27 +145,36 @@ public class LyricsActivity extends Activity {
                 Log.d(TAG, "track is " + track.toString());
                 setTitle(track.title);
                 mTrackTitleView.setText(track.title);
+
+                // set current position
+                mCurrentTrackPos = track.pos;
+                mLastSyncTimestamp = System.currentTimeMillis();
+
                 switch (track.lyric_status) {
                     case Track.LyricStatus.SEARCHING:
                         mMainContainer.setVisibility(View.INVISIBLE);
                         mStateIndicator.setVisibility(View.VISIBLE);
                         mStateIndicator.setText(getResources().getString(R.string.searching));
+                        stopTrackPosition();
                         break;
                     case Track.LyricStatus.FOUND:
                         mMainContainer.setVisibility(View.VISIBLE);
                         mStateIndicator.setVisibility(View.INVISIBLE);
                         Log.d(TAG, track.lyric);
                         setLyric(track.lyric);
+                        startTrackPosition();
                         break;
                     case Track.LyricStatus.NOT_FOUND:
                         mMainContainer.setVisibility(View.INVISIBLE);
                         mStateIndicator.setVisibility(View.VISIBLE);
                         mStateIndicator.setText(getResources().getString(R.string.lyric_not_found));
+                        stopTrackPosition();
                         break;
                     case Track.LyricStatus.ERROR:
                         mMainContainer.setVisibility(View.INVISIBLE);
                         mStateIndicator.setVisibility(View.VISIBLE);
                         mStateIndicator.setText(getResources().getString(R.string.search_error));
+                        stopTrackPosition();
                         break;
                     default:
                         // no default
@@ -227,8 +191,29 @@ public class LyricsActivity extends Activity {
                 Log.d(TAG, "current track subject complete");
             }
         }));
+        mDisposable.add(mApp.mStatusSubject
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableObserver<PlayStatus>() {
+                    @Override
+                    public void onNext(PlayStatus playStatus) {
+                        if (playStatus.status == PowerampAPI.Status.TRACK_PLAYING && !playStatus.isPaused) {
+                            startTrackPosition();
+                        } else {
+                            stopTrackPosition();
+                        }
+                    }
 
-        trackPlayerStatus();
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG, "track error: " + e.toString());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "Track complete");
+                    }
+                }));
+
         super.onResume();
     }
 
@@ -236,7 +221,7 @@ public class LyricsActivity extends Activity {
     protected void onPause() {
         Log.w(TAG, "on pause");
         mDisposable.dispose();
-        stopTrackPlayerStatus();
+        unregisterReceiver(mTrackPosSyncReceiver);
         super.onPause();
     }
 
